b937ef7130f7176452b3db7790423933ee23ac61
Carlos Augusto <carlosfilho.canal@icloud.com>
2022-04-15 22:45:22 -0300

    docs-format

--- estudosClasses.dart
+++ estudosClasses.dart
@@ -1,9 +1,9 @@
 // LEMBRAR DE INICIAR VARIÁVEIS COM LETRA MAIÚSCULA
 
 class Animal {
-  String species;
-  String color;
-  int age;
+  String? species;
+  String? color;
+  int? age;
 
   sleep() {
     print('o animal $species está dormindo');
